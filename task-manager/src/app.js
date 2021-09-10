33f064b44a72a9f8edb1e2957315dbfc55fff89e
AbdallahBedir <abdallahahmed2025@gmail.com>
2021-09-10 21:02:51 +0200

    first commit

--- task-manager/src/app.js
+++ task-manager/src/app.js
@@ -9,4 +9,13 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.use(function (req, res, next) {
+    res.status(404).send({message:"Sorry can't find that!"})
+})
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
 module.exports = app
--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,7 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
+// In config/dev.env
+// MONGODB_URL=mongodb://127.0.0.1:27017/task-manager-api
 
 mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
